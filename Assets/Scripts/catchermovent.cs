f824dd23b9fcd8ad1b6e2a5cbe344583aa170422
game-projectss-admin <shahzadranjha990@gmail.com>
2023-01-19 20:50:39 +0500

    input system applied on player with rigidbody.

--- Assets/Scripts/catchermovent.cs
+++ Assets/Scripts/catchermovent.cs
@@ -1,33 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 public class catchermovent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private int Bound = 10;
-    float horizotal;
+    
+    private int Bound = 10; 
+    Vector2 horizotal = Vector2.zero;
     int speed = 15;
+    private Rigidbody2D rb;
     
+    //Playermovement control by input system
+    public InputAction playerControls;
     
+    //enabling Player Controls
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+    //disable player control
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+    //rigidbody reference passed to script
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    //reading value of inputsystem
     void Update()
     {
-        #region movement depricated by azeem.   (Shahzad)
-        horizotal = Input.GetAxis("Horizontal");
-        transform.Translate(horizotal * Vector2.right* speed * Time.deltaTime);
+        horizotal = playerControls.ReadValue<Vector2>();
+    //bounding player movement to the camera
         if (transform.position.x >= Bound)
         {
             transform.position = new Vector2(Bound, transform.position.y);
 
         }
+
         if (transform.position.x <= -Bound)
         {
             transform.position = new Vector2(-Bound, transform.position.y);
-
         }
-        #endregion
-        //ended region
+    }
+    //setting velocity of player
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizotal.x * speed,horizotal.y*0);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
