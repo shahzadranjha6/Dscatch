3efd70b414599b208ef1a818de679efb515d6846
game-projectss-admin <shahzadranjha990@gmail.com>
2023-01-20 18:33:39 +0500

    Spawning and position of gems

--- Assets/Scripts/SpawnerMovement.cs
+++ Assets/Scripts/SpawnerMovement.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 public class SpawnerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    public string[] Diamonds;
+
+
+    int Bound = 10;
+
+    // Start is called before the first frame update
     void Start()
     {
         MoveRight();
+        InvokeRepeating("SpawnObject", 1, (float)0.7);
+    }
+
+    //--- method to spawn object
+    void SpawnObject()
+    {
+        Vector2 spawnpos = new Vector2(Random.Range(-Bound, Bound), transform.position.y);
+        int RandomIndex = Random.Range(0, Diamonds.Length);
+
+        float RandomScale = Random.Range(0.5f, 1.5f);
+        ObjectPooler.instance.SpawnfromPool(Diamonds[RandomIndex],transform.position, Quaternion.Euler(0, 0, Random.Range(-180, 180))).transform.localScale = new Vector3(RandomScale, RandomScale, 0.5f);
+    
+       
     }
 
     void MoveRight()
