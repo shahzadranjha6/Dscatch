cd54e284c158c3403bf4ed0c68552b738f2ae7db
DESKTOP-DVQ765F\Maaz <saad586305@gmail.com>
2023-01-26 18:38:03 +0500

    touch input added

--- Assets/Scripts/catchermovent.cs
+++ Assets/Scripts/catchermovent.cs
@@ -3,59 +3,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 
 public class catchermovent : MonoBehaviour
 {
     
     private int Bound = 14; 
-    Vector2 horizotal = Vector2.zero;
-    int speed = 25;
+    //horizontal movement for playercontrols for buttons
+    //Vector2 horizotal = Vector2.zero;
+    int speed = 20;
     private Rigidbody2D rb;
-    
-    //Playermovement control by input system
-    public InputAction playerControls;
-    
 
+    //moveleft bool assign to left UI button
+    private bool moveleft = false;
+    //moveright bool assign to right UI button
+    private bool moveright = false;
     
-    //enabling Player Controls
-    private void OnEnable()
-    {
-        playerControls.Enable();
-    }
-    //disable player control
-    private void OnDisable()
-    {
-        playerControls.Disable();
-    }
+    //
+    // //Playermovement control by input system
+    // public InputAction playerControls;
+    //
+    // //enabling Player Controls
+    // private void OnEnable()
+    // {
+    //     playerControls.Enable();
+    // }
+    // //disable player control
+    // private void OnDisable()
+    // {
+    //     playerControls.Disable();
+    // }
     //rigidbody reference passed to script
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    //reading value of inputsystem
+    //reading value of input by canvas button
     void Update()
     {
+        if (moveleft)
+        {
+            leftMove();
+        }
+        else if (moveright)
+        {
+            rightMove();
+        }
+
         // horizotal = playerControls.ReadValue<Vector2>();
-        touchMove();
-    //bounding player movement to the camera
+        // touchMove() //touch move method 
+        
+        //bounding player movement to the camera
         if (transform.position.x >= Bound)
         {
             transform.position = new Vector2(Bound, transform.position.y);
-
+    
         }
-
+    
         if (transform.position.x <= -Bound)
         {
             transform.position = new Vector2(-Bound, transform.position.y);
         }
     }
-    //setting velocity of player
-    // private void FixedUpdate()
-    // {
-    //     rb.velocity = new Vector2(mousePos.x * speed,horizotal.y*0);
-    // }
-
+   
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("BlackToken"))
@@ -74,24 +85,37 @@ public class catchermovent : MonoBehaviour
             }
     }
     //touch movement
-    void touchMove()
+    // void touchMove()
+    // {
+    //     if (Input.GetMouseButton(0))
+    //     {
+    //         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //
+    //         if (mousePos.x < -1)
+    //         {
+    //             //move left
+    //             rb.velocity = new Vector2(-speed ,0);
+    //             
+    //         }
+    //         else if (mousePos.x > 1)
+    //         {
+    //             //move right
+    //             rb.velocity = new Vector2(speed, 0);
+    //         }
+    //         
+    //     }
+    // }
+    
+    //left movement button UI
+     public void leftMove()
     {
-        if (Input.GetMouseButton(0))
-        {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        rb.velocity = new Vector2(-speed,0);
 
-            if (mousePos.x < -1)
-            {
-                //move left
-                rb.velocity = new Vector2(-speed ,0);
-                
-            }
-            else if (mousePos.x > 1)
-            {
-                //move right
-                rb.velocity = new Vector2(speed, 0);
-            }
-            
-        }
+    }
+     //right movement button UI
+    public void rightMove()
+    {
+        rb.velocity = new Vector2(speed, 0);
+
     }
 }
