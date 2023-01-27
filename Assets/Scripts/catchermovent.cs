d867cf8ec82567b438f00de7aa0e316356f402fa
DESKTOP-E5VSA8T\Aleem Ehsan <aleemehsan4@gmail.com>
2023-01-27 21:10:02 +0500

    Right, Left Buttons working Fixed.

--- Assets/Scripts/catchermovent.cs
+++ Assets/Scripts/catchermovent.cs
@@ -9,17 +9,23 @@ using UnityEngine.UIElements;
 public class catchermovent : MonoBehaviour
 {
     
-    private int Bound = 14; 
+    // private int Bound = 14; 
     //horizontal movement for playercontrols for buttons
     //Vector2 horizotal = Vector2.zero;
-    int speed = 20;
     private Rigidbody2D rb;
 
     //moveleft bool assign to left UI button
     private bool moveleft = false;
     //moveright bool assign to right UI button
     private bool moveright = false;
-    
+
+    [Header("Movement Variables")]
+    [Tooltip("Horizontal Speed of the player")]
+    [SerializeField]
+    private int horizontal_Speed;
+    [SerializeField]
+    int speed = 20;
+
     //
     // //Playermovement control by input system
     // public InputAction playerControls;
@@ -38,34 +44,43 @@ public class catchermovent : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        transform.position = new Vector2(0, -3.2853f);  // should spawn from starting Position
     }
     //reading value of input by canvas button
     void Update()
     {
-        if (moveleft)
-        {
-            leftMove();
-        }
-        else if (moveright)
-        {
-            rightMove();
-        }
+        // if (moveleft)
+        // {
+        //     leftMove();
+        // }
+        // else if (moveright)
+        // {
+        //     rightMove();
+        // }
+
+        // rb.AddForce(new Vector2(horizontal_Speed * Time.deltaTime, 0));
 
         // horizotal = playerControls.ReadValue<Vector2>();
         // touchMove() //touch move method 
         
         //bounding player movement to the camera
-        if (transform.position.x >= Bound)
-        {
-            transform.position = new Vector2(Bound, transform.position.y);
+        // if (transform.position.x >= Bound)
+        // {
+        //     transform.position = new Vector2(Bound, transform.position.y);
     
-        }
+        // }
     
-        if (transform.position.x <= -Bound)
-        {
-            transform.position = new Vector2(-Bound, transform.position.y);
-        }
+        // if (transform.position.x <= -Bound)
+        // {
+        //     transform.position = new Vector2(-Bound, transform.position.y);
+        // }
     }
+
+    void FixedUpdate()
+        {
+            rb.AddForce(new Vector2(horizontal_Speed * speed, 0), ForceMode2D.Impulse  );
+        }
    
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -84,6 +99,25 @@ public class catchermovent : MonoBehaviour
                 Debug.Log("collected");
             }
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("BlackToken"))
+        {
+            collision.gameObject.SetActive(false);
+            AudioManager.instance.PlaySound();
+            UIManager.instance.ScoreUpdate();
+            Debug.Log("collected");
+        }
+        else if (collision.gameObject.CompareTag("RedToken"))
+        {
+            collision.gameObject.SetActive(false);
+            AudioManager.instance.PlaySound();
+            UIManager.instance.ScoreUpdateMinus();
+            Debug.Log("collected");
+        }
+    }
+
     //touch movement
     // void touchMove()
     // {
@@ -116,6 +150,15 @@ public class catchermovent : MonoBehaviour
     public void rightMove()
     {
         rb.velocity = new Vector2(speed, 0);
-
     }
+
+
+
+    public void horizontalMovement(int got_Speed)
+        {
+            Debug.Log("horizontal movement called");
+            horizontal_Speed = got_Speed;
+        }
+
+
 }
